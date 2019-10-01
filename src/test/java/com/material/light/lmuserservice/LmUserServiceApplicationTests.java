28bf4ecca66188e347c0ac13dff9a71b32d98e98
DJames <davejames1109@gmail.com>
2019-10-01 12:38:08 +0800

    Added project structure

---
+++ src/test/java/com/material/light/lmuserservice/LmUserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.material.light.lmuserservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LmUserServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
